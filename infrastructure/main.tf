4be7fab10bf18b65846613d915f69882a6f5ec26
Karthik G. Appiah <karthikgappiah@gmail.com>
2026-04-26 16:12:21 -0700

    feat: bootstrap terraform (#1)
    * feat: set up terraform with local state
    
    * test: list all regions
    
    * refactor: migrate to local backend
    
    * refactor: migrate to api key auth
    
    * feat: provision compartment and bucket for remote state
    
    * style: format code
    
    * feat: set up terraform with remote state
    
    * feat: set up github actions
    
    * fix: add subdirectory support
    
    * refactor: migrate to environment secrets
    
    * fix: separate plan and apply actions
    
    * fix: revert to repository secrets

---
+++ infrastructure/main.tf
@@ -0,0 +1,63 @@
+# ------------------------------------------------------------------------------
+# PATH: ~/infrastructure/main.tf
+# ------------------------------------------------------------------------------
+
+# --- Versions ---
+
+terraform {
+  required_version = "~> 1.14.0" # Allows 1.14.X versions only.
+
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 8.5.0" # Allows 8.5.X versions only.
+    }
+  }
+
+  backend "oci" {
+    bucket    = "tfstate_bucket"
+    namespace = "ax99ng5pq6oc"           # Must be hardcoded.
+    key       = "free/terraform.tfstate" # Located at tfstate_bucket/free/terraform.tfstate
+  }
+}
+
+# --- Providers ---
+
+variable "region" { type = string }
+variable "tenancy_ocid" { type = string }
+variable "user_ocid" { type = string }
+variable "private_key_path" { type = string }
+variable "fingerprint" { type = string }
+
+provider "oci" {
+  auth             = "APIKey"
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  private_key_path = var.private_key_path
+  fingerprint      = var.fingerprint
+}
+
+# --- Compartment ---
+
+resource "oci_identity_compartment" "free_compartment" {
+  compartment_id = var.tenancy_ocid
+  name           = "free_compartment"
+  description    = "A compartment for resources in the Always Free Tier."
+
+  enable_delete = true
+}
+
+# --- Remote State ---
+
+data "oci_objectstorage_namespace" "ns" {
+  compartment_id = var.tenancy_ocid
+}
+
+resource "oci_objectstorage_bucket" "tfstate_bucket" {
+  compartment_id = oci_identity_compartment.free_compartment.id
+  name           = "tfstate_bucket"
+
+  namespace  = data.oci_objectstorage_namespace.ns.namespace
+  versioning = "Enabled"
+}
